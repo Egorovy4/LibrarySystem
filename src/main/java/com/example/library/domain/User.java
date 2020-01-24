7c4b6b1d5db782180eac19ececb851e0897016dd
Egorovy4 <mailbyiwanyshyn@gmail.com>
2020-01-24 10:36:05 +0200

    added UserDao, mapped User model

--- src/main/java/com/example/library/domain/User.java
+++ src/main/java/com/example/library/domain/User.java
@@ -1,10 +1,30 @@
 package com.example.library.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "age")
 	private Integer age;
+	
+	@Column(name = "email")
 	private String email;
+	
+	@Column(name = "password")
 	private String password;
 
 	public User() {
